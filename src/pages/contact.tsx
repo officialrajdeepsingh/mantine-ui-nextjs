da12c7ac688b4e391f1600eb0e078da7efe2d9f1
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-01 19:27:58 +0530

    update the contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function contact() {
   return (
-    <div className={`flex min-h-screen flex-col items-center justify-between p-4 md:p-24 `}>
+    <div className={`flex min-h-screen flex-col p-4 md:p-24 `}>
       <h1> Contact us </h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel nibh massa. Suspendisse sit amet porta mauris, viverra semper urna. Vestibulum cursus tincidunt est, non tincidunt tortor lobortis ut. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sed blandit nunc, vitae pellentesque augue. Nunc lacus augue, sollicitudin eget porttitor quis, gravida eu libero. Sed commodo volutpat dui, ac lobortis est dictum eu. In sollicitudin pharetra arcu. Sed accumsan faucibus iaculis. </p>
+      <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel nibh massa. Suspendisse sit amet porta mauris, viverra semper urna. Vestibulum cursus tincidunt est, non tincidunt tortor lobortis ut. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sed blandit nunc, vitae pellentesque augue. Nunc lacus augue, sollicitudin eget porttitor quis, gravida eu libero. Sed commodo volutpat dui, ac lobortis est dictum eu. In sollicitudin pharetra arcu. Sed accumsan faucibus iaculis. </p>
     </div>
   )
 }
