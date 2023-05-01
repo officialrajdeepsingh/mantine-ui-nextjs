052fe76577530ca7b1f2f22f7fe5abe55d71b2b4
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-01 18:38:12 +0530

    remove unused code and creating layout for blog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { HeaderMenu } from '@/components/Header/Header';
+let links = [
+  { link: "/", label: "Home" },
+  { link: "/about", label: "About us" },
+  { link: "/contact", label: "Contact us" },
+
+
+]
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'demo-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }}>
+        <HeaderMenu links={links} />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
