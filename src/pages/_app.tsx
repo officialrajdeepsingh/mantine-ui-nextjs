48ec84679562b719de97c744bb2402795cb0531e
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-01 19:12:44 +0530

    update the links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import { useHotkeys, useLocalStorage } from '@mantine/hooks';
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
 import { HeaderMenu } from '@/components/Header/Header';
-let links = [
-  { link: "/", label: "Home" },
-  { link: "/about", label: "About us" },
-  { link: "/contact", label: "Contact us" },
 
-
-]
+let links = [ { link: "/", label: "Home" }, { link: "/about", label: "About us" }, { link: "/contact", label: "Contact us" }];
 
 export default function App({ Component, pageProps }: AppProps) {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
@@ -19,9 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     getInitialValueInEffect: true,
   });
 
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-
+  const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
