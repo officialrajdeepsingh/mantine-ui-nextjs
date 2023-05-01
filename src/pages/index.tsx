985751dd84b0b2ab1e88469e448c778edb9b4508
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-01 19:12:14 +0530

    update the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { CardsGrid } from "@/components/Card/Card";
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-4 md:p-24 `}>
-      <h1 className="mb-12"> Lets start </h1>
+      <h1 className="mb-12"> Let's start </h1>
       <CardsGrid />
     </main>
   )
